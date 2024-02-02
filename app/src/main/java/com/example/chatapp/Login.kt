ff2598fe0db6455a348c12a0d3320a4b939bbe60
dev05 <dev05@dev.co-mit.com>
2024-02-02 11:43:32 +0700

    login and signup when first db

--- app/src/main/java/com/example/chatapp/Login.kt
+++ app/src/main/java/com/example/chatapp/Login.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatapp.ui.theme.ChatAppTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : ComponentActivity() {
     private  lateinit var edtEmail: EditText
@@ -21,9 +23,13 @@ class Login : ComponentActivity() {
     private  lateinit var btnLogin: Button
     private  lateinit var btnSignUp: Button
 
+    private  lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -35,25 +41,45 @@ class Login : ComponentActivity() {
             startActivity(intent)
         }
         btnLogin.setOnClickListener() {
-            if (edtEmail.text.toString().isEmpty()) {
-                edtEmail.error = "Please enter email"
-                edtEmail.requestFocus()
-                return@setOnClickListener
-            } else if (edtPassword.text.toString().isEmpty()) {
-                edtPassword.error = "Please enter password"
-                edtPassword.requestFocus()
-                return@setOnClickListener
-            } else {
-                if (edtEmail.text.toString() == "duong@gmail.com" && edtPassword.text.toString() == "admin") {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email, password)
+        }
+
+//        btnLogin.setOnClickListener() {
+//            if (edtEmail.text.toString().isEmpty()) {
+//                edtEmail.error = "Please enter email"
+//                edtEmail.requestFocus()
+//                return@setOnClickListener
+//            } else if (edtPassword.text.toString().isEmpty()) {
+//                edtPassword.error = "Please enter password"
+//                edtPassword.requestFocus()
+//                return@setOnClickListener
+//            } else {
+//                if (edtEmail.text.toString() == "duong@gmail.com" && edtPassword.text.toString() == "admin") {
+//                    val intent = Intent(this, MainActivity::class.java)
+//                    startActivity(intent)
+//                } else {
+//                    edtEmail.error = "Email or password is incorrect"
+//                    edtEmail.requestFocus()
+//                    return@setOnClickListener
+//                }
+//            }
+//        }
+    }
+
+    private fun login(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for logging in user
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 } else {
-                    edtEmail.error = "Email or password is incorrect"
-                    edtEmail.requestFocus()
-                    return@setOnClickListener
+                    Toast.makeText(baseContext, "Login failed. Try again after some time.", Toast.LENGTH_SHORT).show()
                 }
             }
-
-        }
     }
 }
+
--- app/src/main/java/com/example/chatapp/SignUp.kt
+++ app/src/main/java/com/example/chatapp/SignUp.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatapp.ui.theme.ChatAppTheme
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : ComponentActivity() {
     private lateinit var edtName: EditText
@@ -21,9 +23,13 @@ class SignUp : ComponentActivity() {
     private  lateinit var edtPassword: EditText
     private  lateinit var btnSignUp: Button
 
+    private  lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtName = findViewById(R.id.edt_name)
         edtEmail = findViewById(R.id.edt_email)
@@ -31,22 +37,43 @@ class SignUp : ComponentActivity() {
         btnSignUp = findViewById(R.id.btnSignup)
 
         btnSignUp.setOnClickListener() {
-            if (edtName.text.toString().isEmpty()) {
-                edtName.error = "Please enter name"
-                edtName.requestFocus()
-                return@setOnClickListener
-            } else if (edtEmail.text.toString().isEmpty()) {
-                edtEmail.error = "Please enter email"
-                edtEmail.requestFocus()
-                return@setOnClickListener
-            } else if (edtPassword.text.toString().isEmpty()) {
-                edtPassword.error = "Please enter password"
-                edtPassword.requestFocus()
-                return@setOnClickListener
-            } else {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
         }
+
+//        btnSignUp.setOnClickListener() {
+//            if (edtName.text.toString().isEmpty()) {
+//                edtName.error = "Please enter name"
+//                edtName.requestFocus()
+//                return@setOnClickListener
+//            } else if (edtEmail.text.toString().isEmpty()) {
+//                edtEmail.error = "Please enter email"
+//                edtEmail.requestFocus()
+//                return@setOnClickListener
+//            } else if (edtPassword.text.toString().isEmpty()) {
+//                edtPassword.error = "Please enter password"
+//                edtPassword.requestFocus()
+//                return@setOnClickListener
+//            } else {
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+//            }
+//        }
+    }
+
+    private fun signUp (email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    //code for jump to home
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(baseContext, "Sign Up failed. Try again after some time.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
+
